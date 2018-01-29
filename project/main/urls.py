a38f003c42abc590e69a0fcfa06ca56752861550
HwangJunPhill <444.gkk@gmail.com>
2018-01-30 00:23:03 +0900

    main page deco and create blog index

--- project/main/urls.py
+++ project/main/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib.auth.views import login, logout
 from . import views
-from .views import IndexView
+from .views import IndexView, WriteView, BlogView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, {'next_page' : '/'}, name='logout'),
+    url(r'^blog/$', BlogView.as_view(), name='blog'),
+    url(r'^blog/write/$', WriteView.as_view(), name='write'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- project/main/views.py
+++ project/main/views.py
@@ -6,3 +6,11 @@ from django.views.generic import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'main/index.html')
+
+class WriteView(View):
+    def get(self, request):
+        return render(request, 'blog/write.html')
+
+class BlogView(View):
+    def get(self, request):
+        return render(request, 'blog/blog.html')
