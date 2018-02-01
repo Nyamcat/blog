1c07ffd69f4838b1e51e55bcd1ceeb1ed2cf1ab1
HwangJunPhill <444.gkk@gmail.com>
2018-02-01 23:11:20 +0900

    edit view, url for blog

--- project/main/views.py
+++ project/main/views.py
@@ -6,11 +6,3 @@ from django.views.generic import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'main/index.html')
-
-class WriteView(View):
-    def get(self, request):
-        return render(request, 'blog/write.html')
-
-class BlogView(View):
-    def get(self, request):
-        return render(request, 'blog/blog.html')
