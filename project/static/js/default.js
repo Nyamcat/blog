fd74405e8b93aa02f653bcdf984febf9c363a9f6
Harper Hwang <444.gkk@gmail.com>
2018-06-11 20:24:09 +0900

    side menu

--- project/static/js/default.js
+++ project/static/js/default.js
@@ -1,5 +1,41 @@
-$(function(){
-    $('#menu_button').click(function(){
-        $('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
+jQuery(function($)
+{
+    $(document).ready(function () {
+
+        $(function(){
+            $('#menu_button').click(function(){
+                $('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
+            });
+        });
+
+        $(".menutitle").click(function(){
+            var submenu = $(this).next("ul");
+            console.log('zzz');
+
+            if( submenu.is(":visible") ){
+                tmp = $(this).text().replace(/\-/g,'+');
+                $(this).text(tmp)
+                submenu.slideUp();
+            }else{
+                tmp = $(this).text().replace(/\+/g,'-');
+                $(this).text(tmp)
+                submenu.slideDown();
+            }
+        });
+
+        //사이드메뉴
+        $('.submenutitle').each(function(idx)
+        {
+            var s = $(this),
+            a = s.find('li>a');
+
+            a.bind('focus mouseenter', function(e)
+            {
+                a.removeClass('active');
+                $(this).addClass('active');
+            });
+        });
+
     });
+
 });
