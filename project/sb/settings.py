a1d175e4aaca62ea8ed3d878acc8cbc95981d405
Hwang JunPhill <444.gkk@gmail.com>
2019-01-23 22:11:25 +0900

    방문자수 통계 middleware 생성

--- project/sb/settings.py
+++ project/sb/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'statis.middleware.VisitorCountMiddleware',
 ]
 
 ROOT_URLCONF = 'sb.urls'
