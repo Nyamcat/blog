3a1c326a9f0912f5f63070f84bf4439e42f36ec9
Hwang JunPhill <444.gkk@gmail.com>
2019-01-24 10:07:41 +0900

    방문자수 예시

--- project/main/views.py
+++ project/main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import View
+from statis.models import Visitor
 
 # Create your views here.
 
+
 class IndexView(View):
     def get(self, request):
-        return render(request, 'main/index.html')
+        total_visit = len(Visitor.objects.all())
+        today_visit = len(Visitor.objects.filter(date=timezone.now().date()))
+        yesterday_visit = len(Visitor.objects.filter(date=timezone.now().date() + timezone.timedelta(days=-1)))
+        context = {'total_visit': total_visit, 'today_visit': today_visit, 'yesterday_visit': yesterday_visit}
+        return render(request, 'main/index.html', context)
 
 
 class AboutView(View):
