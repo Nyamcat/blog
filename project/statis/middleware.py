3588996805221ffbc2dbcba51ced70ce09daa885
Hwang JunPhill <444.gkk@gmail.com>
2019-01-24 09:42:31 +0900

    블로그 방문자 수 통계 기능 완성

--- project/statis/middleware.py
+++ project/statis/middleware.py
@@ -1,3 +1,9 @@
+from ipware.ip import get_ip
+from django.utils import timezone
+
+from .models import Visitor
+
+
 class VisitorCountMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
@@ -8,7 +14,32 @@ class VisitorCountMiddleware(object):
         # the view (and later middleware) are called.
 
         response = self.get_response(request)
-        print(request)
+
+        if request.method == 'GET':
+            ip = get_ip(request)
+            ip_display = ip.split('.')[0] + '.' + ip.split('.')[1] + '. * . *'
+            print(ip)
+
+            try:
+                visitor = Visitor.objects.get(ip=ip, date=timezone.now())
+
+            except Exception as e:
+                # 처음 블로그를 방문한 경우엔 조회 기록이 없음
+                print(e)
+                visitor = Visitor(ip=ip, date=timezone.now(), ip_display=ip_display)
+
+            else:
+                # 방문 기록은 있으나, 날짜가 다른 경우
+                if not visitor.date == timezone.now().date():
+                    visitor = Visitor(ip=ip, date=timezone.now(), ip_display=ip_display)
+                # 날짜가 같은 경우
+                else:
+                    visitor.number_of_get_request = visitor.number_of_get_request + 1
+                    print(str(ip) + ' has already hit this post.\n\n')
+            visitor.save()
+        else:
+            print('Nope')
+
         print(response)
 
         # Code to be executed for each request/response after
